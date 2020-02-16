f7c83cafd52cd49dbd90e01f44b2d9939a8ab4ff
坤 杨 <zhujiyangkun@163.com>
2020-02-16 04:52:01 +0000

    chapter8

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.inclusion_tag('rango/categories.html')
 def get_category_list():
-    return {'categories': Category.objects.all()}
+    return {'categories': Category.objects.all(), 'current_category': current_category}
